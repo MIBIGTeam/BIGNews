506bf7776577996fb4b044e541a045748abe9681
Mislav Javor <mislav.javor@gmail.com>
2014-05-11 00:02:58 +0200

    aesthetic chanfes

--- src/com/hackaton/util/StringModifier.java
+++ src/com/hackaton/util/StringModifier.java
@@ -2,6 +2,6 @@ package com.hackaton.util;
 
 public class StringModifier {		
 	public static String modifyString(String input){		
-		return input.replaceAll("\\s+","").replace('#', ',');
+		return input.replaceAll("\\s+","").replace('#', ',').replaceFirst(",","");
 	}
 }
