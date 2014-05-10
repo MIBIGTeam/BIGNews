cbc7b9f31ee3aadae0dba6596ba4ced9688509a6
Mislav Javor <mislav.javor@gmail.com>
2014-05-10 14:05:54 +0200

    Filled XML files with content. Put placeholder picture (remove it later), added resources in drawable-xxhdbi (to scale later). Added action bar removal to Java files in all classes

--- src/com/hackathon/bigteam/AddNews.java
+++ src/com/hackathon/bigteam/AddNews.java
@@ -3,12 +3,14 @@ package com.hackathon.bigteam;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class AddNews extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_add_news);
 	}
 
