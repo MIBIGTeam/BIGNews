c970845a2c9fa0eec2cb79a482a723c3591c1593
Bojan Lovrovic <bojanlovrovic@gmail.com>
2014-05-10 16:10:30 +0200

    no message

--- src/com/hackaton/util/UrlMaker.java
+++ src/com/hackaton/util/UrlMaker.java
@@ -1,12 +1,12 @@
 package com.hackaton.util;
 
 public class UrlMaker {
-
-	public String prefix;
 	
-	public static String GetArticle() {
+	private static String security = "2e435d9b2aae179ca7f5d3993b0a76c80a17e8cd";
+	
+	public static String GetArticle(int articleID) {
 		
-		return "https://www.google.hr/";
+		return "http://trashnfun.com/News/getArticleById.php?&sigurnost=" + security + "&ArticleId=" + articleID;
 	}
 	
 }
