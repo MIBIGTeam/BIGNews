dfc40d7fb99497389f4cdba350da7e284cfc5294
Bojan Lovrovic <bojanlovrovic@gmail.com>
2014-05-10 19:41:53 +0200

    no message

--- src/com/hackathon/bigteam/MainActivity.java
+++ src/com/hackathon/bigteam/MainActivity.java
@@ -1,45 +1,19 @@
 package com.hackathon.bigteam;
 
-import com.hackaton.util.HttpRequest;
-import com.hackaton.util.InitializingFinished;
-import com.hackaton.util.NextScreen;
-import com.hackaton.util.UrlMaker;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
-	private Button button;
-	private ListView articleList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		  articleList.
 		
-		button.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				NextScreen initializingFinished = new InitializingFinished(ReadNews.class);
-				HttpRequest request = new HttpRequest(MainActivity.this, initializingFinished, 2, true);
-				String url = UrlMaker.GetArticle(3);
-				request.execute(url);
-				Log.i("MainActivity starts url: ", url);
-				
-			}
-		});
+		
 	}
 
 	@Override
